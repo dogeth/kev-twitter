f256ce0d0802d911db7eb978be32c6eb6216631b
Kevin Read <readkev@gmail.com>
2010-04-10 15:01:39 +1000

    Completed add

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,12 @@
 class TweetsController < ApplicationController
   
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all(:order => "created_at desc")
     
+  end
+  
+  def create
+    Tweet.create(params[:tweet])
+    redirect_to tweets_path
   end
 end
